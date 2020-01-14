207cfc12ea7d6fb8cd4a34a47f6022d4175acfdc
Glenn McEwan <glenn@web-dev.ninja>
2020-01-14 18:09:04 +0000

    Added migrations and seeders for initial entities

---
+++ database/migrations/2020_01_12_183111_create_checkins_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCheckinsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkins', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('user_id')->references('id')->on('users');
+            // $table->string('drink_id')->references('id')->on('drinks');
+            $table->string('measure_id')->references('id')->on('measures');
+            $table->string('serving_style_id')->references('id')->on('serving_styles');
+            $table->float('rating');
+            $table->string('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkins');
+    }
+}
