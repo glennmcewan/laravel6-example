749261b8ae6be9e2225d6d1d4f055a745dcd07eb
Glenn McEwan <glenn@web-dev.ninja>
2020-01-19 17:19:25 +0000

    Change checkin link in navbar to point to the create page rather than listing

--- resources/views/layouts/components/nav.blade.php
+++ resources/views/layouts/components/nav.blade.php
@@ -11,7 +11,7 @@
                 <a class="nav-link" href="{{ route('home') }}">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('checkin.index') }}">Check in</a>
+                <a class="nav-link" href="{{ route('checkin.create') }}">Check in</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
